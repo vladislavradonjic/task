1aa5091e67842899cbb4820380fdf328fab9c086
Vladislav Radonjic <vanjar@gmail.com>
2026-02-06 23:32:39 +0100

    Read CLI arguments

--- src/task/cli.py
+++ src/task/cli.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello, World!")
+import sys
+
+
+def main() -> None:
+    args = sys.argv
+    for i, arg in enumerate(args):
+        print(f"{i}: {arg!r}")
+
 
 if __name__ == "__main__":
     main()
