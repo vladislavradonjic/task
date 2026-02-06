4a6fa9f944fad1f60ef4d5446b6ef658b8e6b744
Vladislav Radonjic <vanjar@gmail.com>
2026-02-06 23:33:45 +0100

    Create dummy commands

---
+++ src/task/commands.py
@@ -0,0 +1,16 @@
+"""Command implementations. Dummy commands for now; real logic later."""
+
+
+def add_(filter_args: list[str], modify_args: list[str]) -> None:
+    """Dummy add command."""
+    print("add (dummy)")
+
+
+def list_(filter_args: list[str], modify_args: list[str]) -> None:
+    """Dummy list command."""
+    print("list (dummy)")
+
+
+def init_(filter_args: list[str], modify_args: list[str]) -> None:
+    """Dummy init command."""
+    print("init (dummy)")
